cc8090428866eab5af5536559b1723c6a5b7f10d
Icaro Castelo <i.castelobp@gmail.com>
2025-05-31 16:51:59 -0300

    feat: autenticação + token + config .env

---
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\User;
+use Illuminate\Support\Facades\Validator;
+
+class ApiAuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+
+        $user = User::create($input);
+        $token = $user->createToken('LaravelToken')->accessToken;
+
+        return response()->json(['token' => $token], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('LaravelToken')->accessToken;
+
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+    }
+
+    public function details()
+    {
+        return response()->json(['user' => Auth::user()], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', 'Auth\ApiAuthController@login');
+Route::post('register', 'Auth\ApiAuthController@register');
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('user', 'Auth\ApiAuthController@details');
 });
